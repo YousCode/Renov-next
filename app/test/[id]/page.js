4718e0cb4ec3f7ef58c613698584b4159115d4e1
Youssouph Camara <88164361+YousCode@users.noreply.github.com>
2024-12-13 11:20:12 +0100

    add test

--- app/test/[id]/page.js
+++ app/test/[id]/page.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 import { Page, Text, View, Document, StyleSheet, Image, Font } from '@react-pdf/renderer';
