93bd38b230932437d4da514806a8aef0d87e8a29
Youssouph Camara <88164361+YousCode@users.noreply.github.com>
2025-10-01 21:04:19 +0100

    chore: remove Sendinblue and stub forgot_password route

--- pages/api/auth/forgot_password.js
+++ pages/api/auth/forgot_password.js
@@ -3,7 +3,7 @@ import User from '../../../models/user';
 import crypto from 'crypto';
 import { capture } from '../../../sentry.js'; // Assurez-vous d'avoir cette fonction utilitaire
 import { buildGenericTemplate } from '../../../emails.js'; // Assurez-vous d'avoir cette fonction utilitaire
-import { transacEmailApi } from "../../../sendinblue.js";
+
 
 export default async function handler(req, res) {
   await connectToDatabase();
--- sendinblue.js
+++
@@ -1,8 +0,0 @@
-// sendinblue.js
-import SibApiV3Sdk from "sib-api-v3-sdk";
-
-const client = SibApiV3Sdk.ApiClient.instance;
-const apiKey = client.authentications["api-key"];
-apiKey.apiKey = process.env.SENDINBLUE_API_KEY; // clé depuis tes variables ENV Vercel
-
-export const transacEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
